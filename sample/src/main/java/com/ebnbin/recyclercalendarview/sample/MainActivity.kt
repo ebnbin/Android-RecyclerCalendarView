c25268eefc14a2ef3f15afb8208cbabf4f52621a
Ebn Zhang <ebnbin@gmail.com>
2017-07-02 03:53:15 +0800

    Release version 0.5.1

--- sample/src/main/java/com/ebnbin/recyclercalendarview/sample/MainActivity.kt
+++ sample/src/main/java/com/ebnbin/recyclercalendarview/sample/MainActivity.kt
@@ -1,26 +1,5 @@
 package com.ebnbin.recyclercalendarview.sample
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 
-import com.ebnbin.eb.util.Timestamp
-import com.ebnbin.recyclercalendarview.RecyclerCalendarView
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-
-        val recyclerCalendarView = findViewById(R.id.recycler_calendar_view) as RecyclerCalendarView
-        recyclerCalendarView.setRange(Timestamp.newInstance(2014, 1, 1, true),
-                Timestamp.newInstance(2019, 12, 31, true))
-        recyclerCalendarView.selectDate(Timestamp.newInstance(2017, 6, 20, true), true)
-        recyclerCalendarView.listeners.add(object : RecyclerCalendarView.Listener {
-            override fun onSelected(date: Timestamp) {
-                Toast.makeText(this@MainActivity, date.toString(), Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-}
+class MainActivity : AppCompatActivity()
--- sample/src/main/java/com/ebnbin/recyclercalendarview/sample/SampleApplication.kt
+++ sample/src/main/java/com/ebnbin/recyclercalendarview/sample/SampleApplication.kt
@@ -2,12 +2,4 @@ package com.ebnbin.recyclercalendarview.sample
 
 import android.app.Application
 
-import com.ebnbin.eb.util.EBUtil
-
-class SampleApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        EBUtil.init(BuildConfig.APPLICATION_ID)
-    }
-}
+class SampleApplication : Application()
